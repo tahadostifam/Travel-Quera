21ab9a2f96af1dcba195a4567f3dd548b098bda1
Taha Dostifam <mr.tahadostifam@gmail.com>
2024-01-02 15:02:18 +0330

    only ranking is left on section-3

--- code.sql
+++ code.sql
@@ -14,3 +14,20 @@ select day, count(*) from (
 group by day
 order by count desc;
 
+-- Section3
+select
+	json_extract_path_text(airport_name, 'en') as airport,
+	model as aircraft_model,
+	count(flight_id),
+	ROW_NUMBER() OVER(ORDER BY count(flight_id) desc) rank
+from (
+	select airport_name, model, public.flights.aircraft_code, ticket_flights.flight_id from public.flights
+	left join public.aircrafts_data on public.flights.aircraft_code=public.aircrafts_data.aircraft_code
+	left join public.airports_data
+		on public.flights.arrival_airport=public.airports_data.airport_code
+		or public.flights.departure_airport=public.airports_data.airport_code
+	left join public.ticket_flights on public.ticket_flights.flight_id=public.flights.flight_id
+	where public.flights.status='Arrived'
+)
+group by airport, model
+order by airport, model, rank;
